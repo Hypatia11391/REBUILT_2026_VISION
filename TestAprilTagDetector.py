ee699699ec4ae122bca375407992fb78a441c73e
SkiRanger <ski.ranger.owen@gmail.com>
2025-12-11 15:54:44 -0800

    Edited test detector to include pose detection and id tracking. Also added a constants file and a utils file.

--- TestAprilTagDetector.py
+++ TestAprilTagDetector.py
@@ -19,6 +19,30 @@ detector = apriltag.Detector()
 results = detector.detect(gray)
 print(f"[INFO] {len(results)} total AprilTags detected")
 
+# Approximate camera intrinsics for 1080p image [PLACEHOLDER]
+fx = 1000.0
+fy = 1000.0
+cx = 960.0   # 1920 / 2
+cy = 540.0   # 1080 / 2
+
+cameraMatrix = np.array([
+    [fx,  0, cx],
+    [0,  fy, cy],
+    [0,   0,  1]
+], dtype=np.float32)
+
+distCoeffs = np.zeros(5)
+
+tag_size = 0.1651  # meters
+half = tag_size / 2.0
+
+objPoints = np.array([
+    [-half,  half, 0],
+    [ half,  half, 0],
+    [ half, -half, 0],
+    [-half, -half, 0]
+], dtype=np.float32)
+
 # Loop over the AprilTag detection results
 for r in results:
 	# Extract the bounding box (x, y)-coordinates for the AprilTag and convert each of the (x, y)-coordinate pairs to integers
@@ -27,18 +51,39 @@ for r in results:
 	ptC = (int(ptC[0]), int(ptC[1]))
 	ptD = (int(ptD[0]), int(ptD[1]))
 	ptA = (int(ptA[0]), int(ptA[1]))
+
 	# Draw the bounding box of the AprilTag detection
 	cv2.line(image, ptA, ptB, (0, 255, 0), 2)
 	cv2.line(image, ptB, ptC, (0, 255, 0), 2)
 	cv2.line(image, ptC, ptD, (0, 255, 0), 2)
 	cv2.line(image, ptD, ptA, (0, 255, 0), 2)
+
 	# Draw the center (x, y)-coordinates of the AprilTag
 	(cX, cY) = (int(r.center[0]), int(r.center[1]))
 	cv2.circle(image, (cX, cY), 5, (0, 0, 255), -1)
+	
 	# Draw the tag family on the image
 	tagFamily = r.tag_family.decode("utf-8")
-	cv2.putText(image, tagFamily, (ptA[0], ptA[1] - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-	print("[INFO] tag family: {}".format(tagFamily))
+	tagID = r.tag_id
+	print(tagID, type(tagID))
+	cv2.putText(image, str(tagID), (ptA[0], ptA[1] - 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+	print(f"[INFO] tag family: {tagFamily}")
+
+	# Estimate pose
+	imgPoints = np.array(r.corners, dtype=np.float32)
+	success, rvec, tvec = cv2.solvePnP(objPoints, imgPoints, cameraMatrix, distCoeffs, flags=cv2.SOLVEPNP_IPPE_SQUARE)
+
+	if success:
+		print("Tag:", r.tag_id)
+		print(" rvec:", rvec.ravel())
+		print(" tvec:", tvec.ravel())
+
+        # Convert to rotation matrix if needed
+		R, _ = cv2.Rodrigues(rvec)
+		print(" Rotation matrix:\n", R)
+	else:
+		print("Pose solve failed for tag:", r.tag_id)
+
 # Show the output image after AprilTag detection
 cv2.imshow("Image", image)
 cv2.waitKey(0)
---
+++ VisionUtills.py
@@ -0,0 +1,7 @@
+import numpy as np
+import cv2
+import pupil_apriltags
+
+class Camera:
+    def __init__(self):
+        pass
