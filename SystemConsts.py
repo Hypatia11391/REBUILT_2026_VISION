234699d0c551540977ad54cc0411b8ff13a69902
SkiRanger <ski.ranger.owen@gmail.com>
2025-12-12 11:30:23 -0800

    Started oulining a file system.

---
+++ SystemConsts.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+# Approximate camera intrinsics for 1080p image [PLACEHOLDER]
+fx = 1000.0
+fy = 1000.0
+cx = 960.0   # 1920 / 2
+cy = 540.0   # 1080 / 2
+
+cameraMatrix = np.array([
+    [fx,  0, cx],
+    [0,  fy, cy],
+    [0,   0,  1]
+], dtype=np.float32)
+
+# Distortion coefficients [PLACEHOLDER]
+distCoeffs = np.array([0, 0, 0, 0, 0])
+
+tag_size = 0.1651  # meters
+half = tag_size / 2.0
+
+objPoints = np.array([
+    [-half,  half, 0],
+    [ half,  half, 0],
+    [ half, -half, 0],
+    [-half, -half, 0]
+], dtype=np.float32)
+
+# Poses of all the tags in the arena by ID
+tagPoses = {0: np.zeros((2, 3), dtype = np.float)} # [PLACEHOLDER]
--- VisionUtills.py
+++ VisionUtills.py
@@ -2,6 +2,3 @@ import numpy as np
 import cv2
 import pupil_apriltags
 
-class Camera:
-    def __init__(self):
-        pass
