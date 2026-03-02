4ae61adddf4f90234e35369686b201d3d9d4b4d5
SkiRanger <ski.ranger.owen@gmail.com>
2026-03-02 15:52:40 -0800

    Working on calibration

---
+++ calibration/calibrate_from_vid.py
@@ -0,0 +1,49 @@
+import numpy as np
+import cv2
+
+video_path = "./calibration_video.mp4"
+
+cap = cv2.VideoCapture(video_path) # Edit this to be the name of the calibration video.
+
+if not cap.isOpen():
+    print("Error while opening the video file. Check video file is at" + video_path)
+
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((7*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:7].T.reshape(-1,2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+
+while cap.isOpen():
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (7,7), None)
+
+    # If found, add object points, image points (after refining them)
+    if ret:
+        objpoints.append(objp)
+
+        corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+        imgpoints.append(corners2)
+
+        # Draw and display the corners
+        cv2.drawChessboardCorners(frame, (7,7), corners2, ret)
+    
+    cv2.imshow('Frame', frame)
+
+    if cv2.waitKey(17) == ord('q'):
+        break
+
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+print((ret, mtx, dist, rvecs, tvecs))
