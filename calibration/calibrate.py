bbc327e887f01d4d19df2ec65eb51503aa693a2d
SkiRanger <ski.ranger.owen@gmail.com>
2026-05-25 17:54:27 -0700

    calibration script

--- calibration/calibrate.py
+++ calibration/calibrate.py
@@ -13,8 +13,8 @@ path = args.images
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+objp = np.zeros((7*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:7].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
@@ -37,7 +37,7 @@ for fname in images:
         imgpoints.append(corners2)
 
         # Draw and display the corners
-        cv2.drawChessboardCorners(img, (7,6), corners2, ret)
+        cv2.drawChessboardCorners(img, (7,7), corners2, ret)
         cv2.imshow('img', img)
         cv2.waitKey(0)
 
@@ -48,6 +48,8 @@ ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.sh
 if ret is not None:
     print("Calibration successful!")
     print(" ")
+
+    print(f"retval = {ret}")
     
     print("Distortion matrix:")
     print(mtx)
